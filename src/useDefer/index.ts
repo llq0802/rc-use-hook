b61a8e1c30924835c125b42ec90c11459fe65414
llq-yyds <958614130@qq.com>
2023-08-10 22:30:50 +0800

    fix: 修复部分bug

--- src/useDefer/index.ts
+++ src/useDefer/index.ts
@@ -4,11 +4,14 @@ import { startTransition, useEffect, useRef, useState } from 'react';
  * 分片渲染长列表
  * @description 用于解决渲染时间过长导致白屏问题
  * @param maxFrameCount {number} 最大列表的数量
+ * @default 1_000
  */
 const useDefer = (maxFrameCount: number = 10_00) => {
   const [frameCount, setFrameCount] = useState(0);
   const frameCountRef = useRef(frameCount);
+  // 防止闭包 获取到最新的 frameCount
   frameCountRef.current = frameCount;
+
   const rafId = useRef(0);
 
   useEffect(() => {
