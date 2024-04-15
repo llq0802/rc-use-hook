0eb21ff07710db59258f43a87875adcfa1518618
李岚清 <958614130@qq.com>
2024-04-15 09:53:24 +0800

    docs: 📚 更新 useShow 文档

--- src/useShow/demos/ChildModel.tsx
+++ src/useShow/demos/ChildModel.tsx
@@ -11,40 +11,47 @@ type ChildModelProps = {
  * 子组件
  */
 export default function ChildModel({ modalRef }: ChildModelProps) {
-  const { showRecord, hideRecord, setParentData, open, updateOpen } = useShow(
-    modalRef,
-    {
-      onShow: (data) => {
-        message.info(
-          `父组件调用了onShow并传参数${JSON.stringify(data, null, 2)}`,
-        );
-        console.log('父组件调用了onShow并传参数 ', data);
-      },
-      onHide: (data) => {
-        message.info(
-          `父组件调用了onHide并传参数${JSON.stringify(data, null, 2)}`,
-        );
-
-        console.log('父组件调用了onHide并传参数 ', data);
-      },
+  const {
+    showRecord,
+    hideRecord,
+    setParentData,
+    open,
+    updateOpen,
+    clear,
+    close,
+  } = useShow(modalRef, {
+    onShow: (data) => {
+      message.info(
+        `父组件调用了onShow并传参数${JSON.stringify(data, null, 2)}`,
+      );
+      console.log('父组件调用了onShow并传参数 ', data);
     },
-  );
+    onHide: (data) => {
+      message.info(
+        `父组件调用了onHide并传参数${JSON.stringify(data, null, 2)}`,
+      );
+
+      console.log('父组件调用了onHide并传参数 ', data);
+      clear();
+    },
+  });
 
   const handleOk = () => {
     setParentData({
       title: '我是子组件数据',
       value: 99,
     });
-    updateOpen(false);
+    close();
   };
 
   const handleCancel = () => {
     setParentData(null);
-    updateOpen(false);
+    close();
   };
 
   return (
     <Modal
+      destroyOnClose
       open={open}
       title="这是子组件的弹窗"
       onOk={handleOk}
@@ -52,8 +59,7 @@ export default function ChildModel({ modalRef }: ChildModelProps) {
       okText="点我向父组件传数据"
     >
       <Form
-        name="basic"
-        labelCol={{ span: 5 }}
+        labelCol={{ span: 4 }}
         wrapperCol={{ span: 19 }}
         initialValues={showRecord}
       >
@@ -76,12 +82,12 @@ export default function ChildModel({ modalRef }: ChildModelProps) {
         <Form.Item
           name="remember"
           valuePropName="checked"
-          wrapperCol={{ offset: 5, span: 19 }}
+          wrapperCol={{ offset: 4, span: 19 }}
         >
           <Checkbox>Remember me</Checkbox>
         </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 5, span: 19 }}>
+        <Form.Item wrapperCol={{ offset: 4, span: 19 }}>
           <span>父组件调用 onShow 传过来的值: </span>
           <pre>{JSON.stringify(showRecord, null, 4)}</pre>
         </Form.Item>
--- src/useShow/index.ts
+++ src/useShow/index.ts
@@ -24,16 +24,16 @@ export declare type UseShowOptions<
   T extends Record<string, any> = Record<string, any>,
 > = {
   /** show 触发事件 */
-  onShow?(data: T): void;
+  onShow?(record: T): void;
   /** hide 触发事件 */
-  onHide?: (data?: T) => void;
-  /** 格式化 onShow 的参数 data */
-  showFormart?: (data: T) => any;
-  /** 格式化 onHide 的参数 data */
-  hideFormart?: (data: T) => any;
+  onHide?: (record?: T) => void;
+  /** 格式化 onShow 的参数 record */
+  showFormart?: (record: T) => any;
+  /** 格式化 onHide 的参数 record */
+  hideFormart?: (record: T) => any;
 };
 
-/**用于在子组件 props 的 'funcRef' 的类型*/
+/**用于在子组件 props 的 useShow 的实例的类型*/
 export declare type UseShowInstanceRef<
   T extends Record<string, any> = Record<string, any>,
 > = MutableRefObject<UseShowInstance<T> | undefined>;
@@ -45,12 +45,19 @@ export declare type UseShowResult<T extends Record<string, any>> = {
   showRecord: T | undefined;
   /** 父组件 useShow 实例调用 onHide 事件传入的参数 */
   hideRecord: T | undefined;
+  /** 配合 Modal 或 Drawer 配置 open */
   open: boolean;
+  /**更新 open  */
   updateOpen: (b: boolean) => void;
+  /** 配合 Modal 或 Drawer 触发 onClose 事件 */
+  close: () => void;
+  /**清空传值的数据 */
+  clear: () => void;
 };
 
 /**
  * 父调用子组件方法，并传值更新状态
+ *  - ( 通常用于 Modal 或 Drawer 中 )
  * @param funcRef 任意名称的 useShow 的实例  `( ref 对象 )`
  * @param options  配置项
  * @returns 父组件调用onShow穿过来的值与传给父组件值的方法
@@ -61,11 +68,10 @@ export default function useShow<
   funcRef: UseShowInstanceRef<T>,
   options: UseShowOptions<T>,
 ): UseShowResult<T> {
+  const [open, setOpen] = useState(false);
   const childrenDataRef = useRef<any>();
   const showRecordRef = useRef<any>();
   const hideRecordRef = useRef<any>();
-  const [open, setOpen] = useState(false);
-
   const opsOnShow = options.onShow,
     opsOnHide = options.onHide,
     opsShowFormart = options.showFormart,
@@ -74,15 +80,15 @@ export default function useShow<
   useImperativeHandle(funcRef, () => {
     return {
       onShow(data) {
+        const record = (showRecordRef.current = cloneDeep(data));
         setOpen(true);
-        showRecordRef.current = cloneDeep(data);
-        opsOnShow?.(showRecordRef.current);
+        opsOnShow?.(record);
       },
 
       onHide(data) {
+        const record = (hideRecordRef.current = cloneDeep(data));
         setOpen(false);
-        hideRecordRef.current = cloneDeep(data);
-        opsOnHide?.(hideRecordRef.current);
+        opsOnHide?.(record);
       },
 
       getChildData() {
@@ -92,26 +98,36 @@ export default function useShow<
   });
 
   const setParentData = useCallback(<T = any>(data: T) => {
-    childrenDataRef.current = data;
+    childrenDataRef.current = cloneDeep(data);
   }, []);
 
   const updateOpen = useCallback((o: boolean) => {
     setOpen(o);
   }, []);
 
-  const clear = useCallback(() => {
-    childrenDataRef.current = void 0;
+  const close = useCallback(() => {
+    setOpen(false);
   }, []);
 
+  const clear = useCallback(() => {
+    childrenDataRef.current = void 0;
+    showRecordRef.current = void 0;
+    hideRecordRef.current = void 0;
+  }, []);
+
+  const showRecord = opsShowFormart
+    ? opsShowFormart(showRecordRef.current)
+    : showRecordRef.current;
+  const hideRecord = opsHideFormart
+    ? opsHideFormart(hideRecordRef.current)
+    : hideRecordRef.current;
   return {
     setParentData,
-    showRecord: opsShowFormart
-      ? opsShowFormart(showRecordRef.current)
-      : showRecordRef.current,
-    hideRecord: opsHideFormart
-      ? opsHideFormart(hideRecordRef.current)
-      : hideRecordRef.current,
+    showRecord,
+    hideRecord,
     open,
     updateOpen,
+    close,
+    clear,
   };
 }
