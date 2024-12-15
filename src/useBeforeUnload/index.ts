08a46ddb8b47a27bfa71dc49e8efcdb418c60619
李岚清 <958614130@qq.com>
2024-12-15 20:40:19 +0800

    refactor: 🎨重构 useBeforeUnload 的实现

--- src/useBeforeUnload/index.ts
+++ src/useBeforeUnload/index.ts
@@ -10,35 +10,16 @@ const useBeforeUnload = (
   message: string = '真的要离开吗?',
 ) => {
   useEffect(() => {
-    const prevOnbeforeunload = window.onbeforeunload;
-
-    if (!enabled) {
-      window.onbeforeunload = prevOnbeforeunload;
-      return;
-    }
-
-    // const handler = (event: BeforeUnloadEvent) => {
-    //   event.preventDefault();
-    //   if (message) {
-    //     event.returnValue = message;
-    //   }
-    //   return message;
-    // };
-    // window.addEventListener('beforeunload', handler);
-    // return () => window.removeEventListener('beforeunload', handler);
-
-    window.onbeforeunload = function (event: BeforeUnloadEvent) {
+    if (!enabled) return;
+    const handler = (event: BeforeUnloadEvent) => {
       event.preventDefault();
-      if (prevOnbeforeunload) {
-        prevOnbeforeunload?.(event);
+      if (message) {
+        event.returnValue = message;
       }
-      event.returnValue = message;
       return message;
     };
-
-    return () => {
-      window.onbeforeunload = prevOnbeforeunload;
-    };
+    window.addEventListener('beforeunload', handler);
+    return () => window.removeEventListener('beforeunload', handler);
   }, [message, enabled]);
 };
 
