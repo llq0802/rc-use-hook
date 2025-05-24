e6f16f324f84de0c9cf8d43619aefbd0907042ad
llq-yyds <958614130@qq.com>
2025-05-24 11:06:12 +0800

    refactor(useScale): 重构useScale逻辑并更新文档
    重构useScale逻辑，移除不必要的flush调用，并添加clientWidth和clientHeight选项。同时更新文档，删除demo1.tsx文件，并修正useScale的描述和示例代码。

--- src/useScale/index.md
+++ src/useScale/index.md
@@ -5,13 +5,7 @@ toc: content
 
 # useScale
 
-判断输入组件是否聚焦
-
-## 代码演示
-
-### 基础用法
-
-<code src="./demo1.tsx" ></code>
+自适应缩放的布局，用于确保固定尺寸的内容在不同大小的屏幕上能够完整显示并保持比例。
 
 ## API
 
@@ -19,15 +13,43 @@ toc: content
 import { useScale } from 'rc-use-hooks';
 ```
 
+### 基本用法
+
+```tsx|pure
+import { useScale } from 'rc-use-hooks';
+import { useRef } from 'react';
+
+export default () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useScale(containerRef, {
+    designWidth: 1920,
+    designHeight: 1080,
+    transition: 'transform 0.3s',
+  });
+
+  return (
+    <div ref={containerRef} style={{ width: 1920, height: 1080 }}>
+      {/* 你的内容 */}
+    </div>
+  );
+};
+```
+
 ### Params
 
-|     参数     |     说明     |                              类型                              | 默认值  |
-| :----------: | :----------: | :------------------------------------------------------------: | :-----: |
-|    target    |   dom节点    | `MutableRefObject<HTMLElement \| null> \| (() => HTMLElement)` |   `-`   |
-| defaultState | 初始是否聚焦 |                           `boolean`                            | `false` |
+|  参数   |  说明   |                              类型                              | 默认值 |
+| :-----: | :-----: | :------------------------------------------------------------: | :----: |
+| target  | dom节点 | `MutableRefObject<HTMLElement \| null> \| (() => HTMLElement)` |  `-`   |
+| options |  配置   |                           `Options`                            |  `-`   |
 
-### Result
-
-|  参数   |   说明   |   类型    | 默认值 |
-| :-----: | :------: | :-------: | :----: |
-| focused | 是否聚焦 | `boolean` |  `-`   |
+```ts
+interface Options {
+  clientWidth?: number;
+  clientHeight?: number;
+  designWidth?: number;
+  designHeight?: number;
+  wait?: number;
+  transition?: string;
+}
+```
--- src/useScale/index.ts
+++ src/useScale/index.ts
@@ -3,40 +3,41 @@ import { getTargetElement } from 'rc-use-hooks/utils';
 import { useLayoutEffect } from 'react';
 
 function useScale(
-  target?: Parameters<typeof getTargetElement>[0],
+  target: Parameters<typeof getTargetElement>[0],
   opt?: {
+    clientWidth?: number;
+    clientHeight?: number;
     designWidth?: number;
     designHeight?: number;
     wait?: number;
     transition?: string;
   },
 ) {
-  const { run, flush } = useDebounceFn(
-    () => {
-      const dom = getTargetElement(target!);
-      if (!dom) return;
-      const designWidth = opt?.designWidth || 1920;
-      const designHeight = opt?.designHeight || 1080;
-      const width = window.innerWidth;
-      const height = window.innerHeight;
-      const scale = Math.min(width / designWidth, height / designHeight);
-      dom.style.transformOrigin = '0 0';
-      dom.style.transition = opt?.transition || 'transform 0.5s';
-      const left = (width - designWidth * scale) / 2;
-      const top = (height - designHeight * scale) / 2;
-      dom.style.transform = `translate(${left}px, ${top}px) scale(${scale})`;
-    },
-    {
-      wait: opt?.wait ?? 500,
-    },
-  );
+  const fn = () => {
+    const dom = getTargetElement(target!);
+    if (!dom) return;
+    const designWidth = opt?.designWidth || 1920;
+    const designHeight = opt?.designHeight || 1080;
+    const width = opt?.clientWidth || window.innerWidth;
+    const height = opt?.clientHeight || window.innerHeight;
+    const scale = Math.min(width / designWidth, height / designHeight);
+    dom.style.transformOrigin = '0 0';
+    dom.style.transition = opt?.transition || 'transform 0.5s';
+    const left = (width - designWidth * scale) / 2;
+    const top = (height - designHeight * scale) / 2;
+    dom.style.transform = `translate(${left}px, ${top}px) scale(${scale})`;
+  };
+
+  const { run } = useDebounceFn(fn, {
+    wait: opt?.wait ?? 0,
+  });
 
   useEventListener('resize', () => {
     run();
   });
 
   useLayoutEffect(() => {
-    flush();
+    fn();
   }, []);
 }
 
